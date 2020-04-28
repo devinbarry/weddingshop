77dcd98a6e6f29a558ed6d219ea839579bef787a
Devin Barry <devinbarry@gmail.com>
2020-04-28 19:39:17 +0100

    Spelling, naming.

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from rover import RoverSim, Direction
 
 
-class TestClosedOrders(TestCase):
+class TestRoverSim(TestCase):
 
     def test_valid_sim_config(self):
         sim = RoverSim(0, 0, Direction.S, 10, 10)
@@ -25,12 +25,12 @@ class TestClosedOrders(TestCase):
     def test_rover_move(self):
         sim = RoverSim(0, 0, Direction.N, 10, 10)
         self.assertEqual(str(sim), "0 0 Direction.N")
-        sim.command("MMMMM")  # Move 5 spaces in the postive y direction
+        sim.command("MMMMM")  # Move 5 spaces in the positive y direction
         self.assertEqual(str(sim), "0 5 Direction.N")
 
     def test_smallest_grid(self):
         # This config dissallows the rover from moving
         sim = RoverSim(0, 0, Direction.N, 0, 0)
         self.assertEqual(str(sim), "0 0 Direction.N")
-        sim.command("MMMMM")  # Move 5 spaces in the postive y direction
+        sim.command("MMMMM")  # Move 5 spaces in the positive y direction
         self.assertEqual(str(sim), "0 0 Direction.N")
